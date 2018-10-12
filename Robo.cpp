ceead0e3db1806a36f32a55f40c70be766e73b11
LaerkeWorkBoeg <44087213+LaerkeWorkBoeg@users.noreply.github.com>
2018-10-12 13:00:39 +0200

    Update Robo.cpp

--- Robo.cpp
+++ Robo.cpp
@@ -10,7 +10,12 @@ int main(){
 
 Mat image = imread("map.PNG", CV_LOAD_IMAGE_GRAYSCALE);
 
-//Hej med jer, Lærkes ændring
+//Finde ud af hvad classer skal indeholde
+  //Robotclass indeholder: Points: start + now og vector<Points> objekter der er detekteret
+//Laves bruschfire på map
+//Følg vej rundt rundt i map
+//Lav en form for laser scanner
+//Find en rute på vej tilbage
   
 
 
