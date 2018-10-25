576ccfa58af1bf1e3196d02373b1a4db1b4b0c3b
JensHesel <44087249+JensHesel@users.noreply.github.com>
2018-10-25 13:20:06 +0200

    Update Robo.cpp

--- Robo.cpp
+++ Robo.cpp
@@ -25,11 +25,16 @@ Mat image = imread("map.PNG", CV_LOAD_IMAGE_GRAYSCALE);
 
 //Mine noter omkring bruschfire algoritmen
 
-/*Hvis man skulle implementere det kan man lave en kode hvor man løber gennem billedet, indtil alle værdier er forskellige fra baggrundsfarven. Hver gang, man støder ind i noget undervejs, farver man det pixel man står på en værdi højere.
+/*Hvis man skulle implementere det kan man lave en kode hvor man løber gennem billedet, 
+indtil alle værdier er forskellige fra baggrundsfarven. Hver gang, man støder ind i noget undervejs, 
+farver man det pixel man står på en værdi højere.
 Denne er O(nm*max(m,n)) (den kan potentielt løbe gennem billedet mange gange)
 
 Der er også en mindre computional tung måde at gøre det på.
-Her starter man med at oprette en naboliste. Så løber man hele biledet igennem, og lægger coordinaterne til alle naboerne i listen. Derefter har den en løkke, indtil der ikke er flere naboer. Der laver man en ny liste med naboer. Så løber man alle de første naboer igennem, og lægger alle deres naboer i den nye liste.
+Her starter man med at oprette en naboliste. Så løber man hele biledet igennem, 
+og lægger coordinaterne til alle naboerne i listen. 
+Derefter har den en løkke, indtil der ikke er flere naboer. 
+Der laver man en ny liste med naboer. Så løber man alle de første naboer igennem, og lægger alle deres naboer i den nye liste.
 Denne er O(nm+nm) (den løber højest billede igennem 1 gang. */
 
   return 0;
