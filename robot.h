55f9be85ef6076444a8aba76789d5e3be446924c
LaerkeWorkBoeg <44087213+LaerkeWorkBoeg@users.noreply.github.com>
2018-11-09 15:01:46 +0100

    Update robot.h

--- robot.h
+++ robot.h
@@ -11,13 +11,14 @@ public:
 	robot(Point begin, Point pos, vector<Point> dots);
 	~robot();
 	void followRute(Mat &input); //Jens
-	Point laserScan(Mat &map, Vec3b color, Point start); //Lærke kigger på den
 	void getBack(Mat &input, Mat &output); //Asger
 	void getToTarget(Mat &map); //Asger
 
 private: 
 	Point now, start;
 	vector<Point> found_targets;
+	Point laserScan(Mat &map, Vec3b color, Point start); //Lærke kigger på den
+	void moveOne(Mat &map, int &moved, Point &old, Vec3b color, bool checkold);
 
 };
 
