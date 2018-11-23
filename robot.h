539bf8bda75d2a8308123ef1bc0af7c68e979818
LaerkeWorkBoeg <44087213+LaerkeWorkBoeg@users.noreply.github.com>
2018-11-23 14:39:28 +0100

    Update robot.h

--- robot.h
+++ robot.h
@@ -11,21 +11,24 @@ public:
 	robot(Point begin, Point pos, vector<Point> dots);
 	~robot();
 	void followRute(Mat &input); //Jens
-	void getBack(Mat &input, Mat &output); //Asger
-	void getToTarget(Mat &map); //Asger
-	Point laserScan(Mat &map, Vec3b mincolor, Vec3b maxcolor, Point from); //Lærke kigger på den, Den skal være i private
-	//Lav søgeområdet rundt i stedet for firkantet
+
+	
+	
 
 	
 
 private: 
 	Point now, start;
 	vector<Point> found_targets;
-	//Point laserScan(Mat &map, Vec3b mincolor, Vec3b maxcolor, Point start); //Lærke kigger på den
-	void moveOne(Mat &map, int &moved, Point &old, Vec3b color, bool checkold);
-	void colvec(Mat &map, vector<Point> points, Vec3b color);
-	bool clearline(Mat &map, Point A, Point B); //returnerer false hvis der er noget sort mellem de to punkter og ellers true
-	//Lav funktion der farver hele det fundende objekt til en anden farve
+	list<Point> path;
 
+	void moveOne(Mat &map, int &moved, Point &old, Vec3b color, bool checkold);
+	void colvec(Mat &map, vector<Point> &points, vector<Vec3b> &color, vector<Vec3b> &prevcolor);
+	bool clearline(Mat &map, Point A, Point B); //returnerer false hvis der er noget sort mellem de to punkter og ellers true
+	void getToTarget(Mat &map, Point dotPos); //Asger
+	void colorTarget(Mat map, Vec3b minColor, Vec3b maxColor); //Funktion der farver hele det fundende objekt til en anden farve
+	Point laserScan(Mat &map, Vec3b mincolor, Vec3b maxcolor); //Lærke kigger på den, Den skal være i private
+															   //Lav søgeområdet rundt i stedet for firkantet
+	void getBack(Mat &map, Point robot_leave); //Asger
 };
 
